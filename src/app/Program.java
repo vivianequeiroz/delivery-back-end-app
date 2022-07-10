29989dcd6648a674e6eb7d7c71031e0436b2eacb
Viviane Queiroz <vivianedsqueiroz@gmail.com>
2022-07-10 18:01:03 -0300

    feat: add sql query to bring all orders

--- src/app/Program.java
+++ src/app/Program.java
@@ -18,36 +18,44 @@ public class Program {
 
 		Statement st = conn.createStatement();
 
-		ResultSet rs = st.executeQuery("select * from tb_order");
+		ResultSet rs = st.executeQuery("SELECT * FROM tb_order "
+				+ "INNER JOIN tb_order_product ON tb_order.id = tb_order_product.order_id "
+				+ "INNER JOIN tb_product ON tb_product.id = tb_order_product.product_id");
 
 		while (rs.next()) {
 
 			// Product product = instantiateProduct(rs);
 
-			Order order = instantiateOrder(rs);
+			Order orders = instantiateOrder(rs);
 
-			System.out.println(order);
+			System.out.println(orders);
 		}
 	}
 
 	private static Product instantiateProduct(ResultSet rs) throws SQLException {
+
 		Product product = new Product();
 		product.setId(rs.getLong("Id"));
 		product.setName(rs.getString("name"));
 		product.setPrice(rs.getDouble("price"));
 		product.setDescription(rs.getString("description"));
 		product.setImageUri(rs.getString("image_uri"));
+
 		return product;
+
 	}
 
 	private static Order instantiateOrder(ResultSet rs) throws SQLException {
+
 		Order order = new Order();
 		order.setId(rs.getLong("Id"));
 		order.setLatitude(rs.getDouble("latitude"));
 		order.setLongitude(rs.getDouble("longitude"));
 		order.setMoment(rs.getTimestamp("moment").toInstant());
 		order.setStatus(OrderStatus.values()[rs.getInt("status")]);
+
 		return order;
+
 	}
 
 }
